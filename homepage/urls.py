eb50c159d1fa4431e22e67e1622fb6343dc314f3
Mike2015 <olumichael2015@outlook.com>
2021-10-20 22:15:41 +0100

    'bruteforce'

---
+++ homepage/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+from . import views
+
+
+urlpatterns = [
+    path('', views.home_page, name='home_page'),
+    path('contact', views.contact, name='contact'),
+    path('newsletter', views.news_letter, name='news'),
+]
